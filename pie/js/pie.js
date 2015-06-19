a7e7ed3380eabb882cf838774e7e23b1d988f433
bayreporta <bayreporta@gmail.com>
2015-06-18 17:31:17 -0700

    pie labels

--- pie/js/pie.js
+++ pie/js/pie.js
@@ -1,9 +1,11 @@
 var pieChart = {
 	vars:{
 		data:[],
-		labels:[],
+		label:[],
 		w:null,
 		h:null,
+		r:null,
+		labelr:null,
 		color:null,
 		contain:null,
 		outRad: null,
@@ -11,21 +13,23 @@ var pieChart = {
 	},
 	getPieAttr: function(p, path, contain, w, h, color, inRad){
 		jQuery.getJSON(path + '.json',function(d){
-			p.label = d[0];
+			p.label = d[2];
 			p.data = d[1];
 			p.w = w;
 			p.h = h;
+			p.r = Math.min(w, h) / 2;
+			p.labelr = p.r + 30;
 			p.color = color;
 			p.contain = contain;
 			p.outRad = p.w / 2
 			p.inRad = inRad;
+
 			pieChart.drawChart(pieChart.vars);
 		})
 	},
 	drawChart: function(p){
 		var arc = d3.svg.arc().innerRadius(p.inRad).outerRadius(p.outRad);
-
-		var pie = d3.layout.pie();
+		var pie = d3.layout.pie().sort(null);
 
 		var color = d3.scale.ordinal().range(p.color);
 
@@ -33,9 +37,17 @@ var pieChart = {
 
 		var arcs = 	svg.selectAll("g.arc").data(pie(p.data)).enter().append("g").attr("class", "arc").attr("transform", "translate(" + p.outRad + "," + p.outRad + ")");
 					arcs.append("path").attr("fill", function(d, i) {return p.color[i];}).attr("d", arc);
+					arcs.append("svg:text").attr("transform", function(d) {
+						var c = arc.centroid(d),
+				            x = c[0],
+				            y = c[1],
+				            // pythagorean theorem for hypotenuse
+				            h = Math.sqrt(x*x + y*y);
+				        return "translate(" + (x/h * p.labelr) +  ',' + (y/h * p.labelr) +  ")"; 
+				    })
+				    .attr("dy", ".35em")
+				    .attr("text-anchor", function(d) {return (d.endAngle + d.startAngle)/2 > Math.PI ? "end" : "start";})
+				    .data(p.label).attr('fill', '#666').text(function(d, i) {return d; });
 	}
 }
 
-window.onload = function(){
-	pieChart.getPieAttr(pieChart.vars, 'data/data','#pie', 300, 300, colorbrewer.Set2[3], 50);
-}
