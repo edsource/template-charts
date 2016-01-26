e83e398077d48a6fbd66ff1282785de8c5fc52c7
bayreporta <bayreporta@gmail.com>
2016-01-26 11:23:31 -0800

    updating table code

--- wp-shortcode.php
+++ wp-shortcode.php
@@ -1,13 +1,15 @@
 <?php 
 
+/* TEMPLATED CHARTS 
+================================================*/
 function chart_shortcode($attr){
-	$attr = shortcode_atts(array('type'=>null, 'data'=>null, 'contain'=>null,'width'=>450,'height'=>300, 'color'=>'BuPu', 'm'=>null,'items'=>null, 'sort'=>false, 'format'=>null, 'ticks'=>5,'padding'=>.1, 'ylabel'=>null, 'xlabel'=>null, 'title'=>null, 'subhed'=>null,'source'=>null, 'max'=>null, 'inrad'=>null, 'legend'=>null), $attr, 'chart');
+	$attr = shortcode_atts(array('class'=>null, 'type'=>null, 'data'=>null, 'contain'=>null,'width'=>450,'height'=>300, 'color'=>'BuPu', 'm'=>null,'items'=>null, 'sort'=>false, 'format'=>null, 'ticks'=>5,'padding'=>.1, 'ylabel'=>null, 'xlabel'=>null, 'title'=>null, 'subhed'=>null,'source'=>null, 'max'=>null, 'inrad'=>null, 'legend'=>null), $attr, 'chart');
 	/* IF CHART NULL, ESCAPE */
 	if ($attr['type'] == null || $attr['contain'] == null || $attr['data'] == null ){return;}
 
 	/* MARGIN MAGIC */
 	if ($attr['m'] == null){
-		$attr['margin'] = [50,100,30,40];
+		$attr['margin'] = [50,40,30,50];
 	}
 	else {
 		$attr['margin'] = explode(',', $attr['m']);
@@ -22,16 +24,16 @@ function chart_shortcode($attr){
 	$output .= "<script type=\"text/javascript\">var chartAttr = new Object();chartAttr = $encode;</script>";
 	switch($attr['type']){
 		case 'stackedbarpercent':
-			$output .= '<div class="temp-charts" id="'.$attr['contain'].'"></div>';
+			$output .= '<div class="temp-charts" role="stackedbarpercent" id="'.$attr['contain'].'"></div>';
 			$output .= "<script type=\"text/javascript\">stackedColumnChart.getAttr(chartAttr.type, chartAttr.data, chartAttr.contain, chartAttr.width, chartAttr.height, chartAttr.margin, colorbrewer[chartAttr.color], chartAttr.items, chartAttr.sort, chartAttr.xlabel, chartAttr.ylabel, chartAttr.format, chartAttr.ticks, chartAttr.padding, chartAttr.title,chartAttr.subhed,chartAttr.source);</script>";
 			break;
 		case 'stackedbar':
-			$output .= '<div class="temp-charts" id="'.$attr['contain'].'"></div>';
+			$output .= '<div class="temp-charts" role="stackedbar" id="'.$attr['contain'].'"></div>';
 			$output .= "<script type=\"text/javascript\">stackedColumnChart.getAttr(chartAttr.type, chartAttr.data, chartAttr.contain, chartAttr.width, chartAttr.height, chartAttr.margin, colorbrewer[chartAttr.color], chartAttr.items, chartAttr.sort, chartAttr.xlabel, chartAttr.ylabel, chartAttr.format, chartAttr.ticks, chartAttr.padding, chartAttr.title,chartAttr.subhed,chartAttr.source);</script>";
 			break;
 		case 'bar':
-			$output .= '<div class="temp-charts" id="'.$attr['contain'].'"></div>';
-			$output .= "<script type=\"text/javascript\">barChart.getAttr(chartAttr.type, chartAttr.data, chartAttr.contain, chartAttr.width, chartAttr.height, chartAttr.margin, chartAttr.color, chartAttr.sort, chartAttr.xlabel, chartAttr.ylabel, chartAttr.format, chartAttr.ticks, chartAttr.padding, chartAttr.title,chartAttr.subhed,chartAttr.source, chartAttr.max);</script>";
+			$output .= '<div class="temp-charts" role="bar" id="'.$attr['contain'].'"></div>';
+			$output .= "<script type=\"text/javascript\">barChart.getAttr(chartAttr.type, chartAttr.data, chartAttr.contain, chartAttr.width, chartAttr.height, chartAttr.margin, chartAttr.color, chartAttr.sort, chartAttr.xlabel, chartAttr.ylabel, chartAttr.format, chartAttr.ticks, chartAttr.padding, chartAttr.title,chartAttr.subhed,chartAttr.source, chartAttr.max, chartAttr.class);</script>";
 			break;
 		case 'pie':
 			$output .= '<section data="#'.$attr['contain'].'" class="pie-contain ed-chart">';
@@ -41,7 +43,7 @@ function chart_shortcode($attr){
 			$output .= '<p><em></em></p></div>';
 			$output .= '<hr class="thick" />';
 			$output .= '<div class="pie-legend"></div>';
-			$output .= '<div class="temp-charts" id="'.$attr['contain'].'"></div>';
+			$output .= '<div id="'.$attr['contain'].'"></div>';
 			$output .= "<script type=\"text/javascript\">pieChart.getAttr(chartAttr.type, chartAttr.data, chartAttr.contain, chartAttr.width, chartAttr.height, chartAttr.margin, chartAttr.padding, chartAttr.color, chartAttr.inrad, chartAttr.title,chartAttr.subhed,chartAttr.source,chartAttr.legend);</script>";
 			$output .= '</section>';
 	}
@@ -49,8 +51,10 @@ function chart_shortcode($attr){
 }
 add_shortcode('chart', 'chart_shortcode');
 
+/* TEMPLATED TABLES
+================================================*/
 function table_shortcode($attr){
-	$attr = shortcode_atts(array('data'=>null, 'contain'=>null, 'format'=>null, 'search'=>false, 'rowname'=>null, 'title'=>null, 'subhed'=>null,'source'=>null,'sort'=>'0,0', 'links'=>null), $attr, 'chart');
+	$attr = shortcode_atts(array('data'=>null, 'contain'=>null, 'format'=>null, 'search'=>false, 'rowname'=>null, 'title'=>null, 'subhed'=>null,'source'=>null,'sort'=>'0,0', 'links'=>null, 'truncate'=>null, 'align'=>null), $attr, 'chart');
 
 	/* IF CHART NULL, ESCAPE */
 	if ($attr['contain'] == null || $attr['data'] == null ){return;}
@@ -78,7 +82,7 @@ function table_shortcode($attr){
 	}
 
 	$output .=  '<div id="'.$attr['contain'].'"></div></section>';
-	$output .= '<script type="text/javascript">prettyTables.getAttr(chartAttr.data, chartAttr.contain, chartAttr.sort, chartAttr.search, chartAttr.title,chartAttr.subhed,chartAttr.source, chartAttr.rowname, chartAttr.format, chartAttr.links);</script>';
+	$output .= '<script type="text/javascript">prettyTables.getAttr(chartAttr.data, chartAttr.contain, chartAttr.sort, chartAttr.search, chartAttr.title,chartAttr.subhed,chartAttr.source, chartAttr.rowname, chartAttr.format, chartAttr.links, chartAttr.truncate, chartAttr.align);</script>';
 	
 	return $output;
 }
